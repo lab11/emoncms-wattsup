4449c394fa2e23e4ff95219b56da1e087e156a3b
Brad Campbell <bradjc5@gmail.com>
2015-12-06 03:27:25 -0500

    copied a bunch of input code

--- wattsup_controller.php
+++ wattsup_controller.php
@@ -28,6 +28,73 @@ function wattsup_controller()
         $result = 'text!';
     }
 
+    if ($route->action == 'post' && $route->format == 'text') {
+        // This looks like a correctly configured Watts Up? .net POST
+
+        $valid = true;
+        $error = '';
+        $userid = $session['userid'];
+        $dbinputs = $input->get_inputs($userid);
+
+        // id is set to the Watts Up? device ID
+        $nodeid = preg_replace('/[^\p{N}\p{L}_\s-.]/u', '', get('id'));
+
+        // Make sure we can do this. Copied from input_controller.php
+        $validate_access = $input->validate_access($dbinputs, $nodeid);
+        if (!$validate_access['success']) {
+            $valid = false;
+            $error = $validate_access['message'];
+        } else {
+            // Insert this record into the emoncms format
+            $time = time();
+
+            // Array to store the relevant fields in
+            $data = array();
+
+            $watts = get('w') / 10;
+            data['watts'] = $watts;
+
+            // Iterate all new data items to insert
+            $tmp = array();
+            foreach ($data as $name => $value) {
+                // Check if this is an existing field in this node or not
+                if (!isset($dbinputs[$nodeid][$name])) {
+                    // New field. 
+                    $inputid = $input->create_input($userid, $nodeid, $name);
+                    $dbinputs[$nodeid][$name] = true;
+                    $dbinputs[$nodeid][$name] = array('id'=>$inputid, 'processList'=>'');
+                    $input->set_timevalue($dbinputs[$nodeid][$name]['id'], $time, $value);
+                } else {
+                    // Existing field, just insert
+                    $input->set_timevalue($dbinputs[$nodeid][$name]['id'], $time, $value);
+
+                    // If there are processes listening to this field, we need
+                    // to pass the data to those as well
+                    if ($dbinputs[$nodeid][$name]['processList']) {
+                        $tmp[] = array('value'=>$value,
+                                       'processList'=>$dbinputs[$nodeid][$name]['processList'],
+                                       'opt'=>array('sourcetype'=>"WATTSUP",
+                                                    'sourceid'=>$dbinputs[$nodeid][$name]['id']));
+                    }
+                }
+            }
+
+            // Actuall insert all of the data to the process
+            foreach ($tmp as $i) {
+                $process->input($time, $i['value'], $i['processList'], $i['opt']);
+            }
+
+        }
+
+        if ($valid) {
+            $result = 'ok';
+        } else {
+            $result = "Error: $error\n";
+        }
+    }
+
+
+
 
 
     
